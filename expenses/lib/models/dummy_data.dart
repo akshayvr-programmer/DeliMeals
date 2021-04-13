64a00d6698a1968634be738a22e34410c64c8e28
Akshay V.R <akshayvr2@gmail.com>
2021-04-13 10:55:02 +0530

    Initial Commit

---
+++ expenses/lib/models/dummy_data.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import './category.dart';
+const  DUMMY_CATEGORIES =  const [
+  Category(id: 'c1', title: 'Italian', color: Colors.purple,),
+  Category(id: 'c2', title: 'Quick and Easy', color: Colors.red,),
+  Category(id: 'c3', title: 'Indian', color: Colors.amber,),
+  Category(id: 'c4', title: 'Hamburgers', color: Colors.orange, ),
+  Category(id: 'c5', title: 'French', color: Colors.pink, ),
+  Category(id: 'c6', title: 'Summer', color: Colors.teal, ),
+  Category(id: 'c7', title: 'Cold Cuts', color: Colors.lightBlueAccent, ),
+  Category(id: 'c8', title: 'Asian', color: Colors.lightGreen, ),
+
+  
+];
